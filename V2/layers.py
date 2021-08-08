70159274ce489deeffeaf8b801ee19506dfa7fb1
ifanzilka <nabiyarov.fanzil@gmail.com>
2021-08-08 17:40:57 +0300

    add conv2

--- V2/layers.py
+++ V2/layers.py
@@ -152,3 +152,71 @@ class Dense(Layer):
 		if self.dropout < 1.0:
 			self.operations.append(Dropout(self.dropout))
 		return None
+
+# Читатели, знакомые с понятием вычислительной сложности, могут
+# сказать, что такой код катастрофически медленный: для вычисления
+# градиента параметра пришлось написать семь вложенных циклов! В этом
+# нет ничего плохого, поскольку нам нужно было прочувствовать и понять
+# принцип работы CNN, написав все с нуля. Но можно написать по-другому:
+# 1) из входных данных извлекаются участки image_height × image_
+# width × num_channels размера filter_height × filter_width из набора
+# тестов;
+# 2) для каждого участка выполняется скалярное произведение на соответствующий фильтр, соединяющий входные каналы с выходными
+# каналами;
+# 3) складываем результаты скалярных произведений, чтобы сформировать результат.
+class Conv2D(Layer):
+    '''
+    Как только мы определим все операции и контур слоя,
+    все, что остается реализовать здесь, - это функция _setup_layer!
+    '''
+    def __init__(self,
+                 out_channels: int,
+                 param_size: int,
+                 dropout: int = 1.0,
+                 weight_init: str = "normal",
+                 activation: Operation = Linear(),
+                 flatten: bool = False) -> None:
+        super().__init__(out_channels)
+        self.param_size = param_size
+        self.activation = activation
+        self.flatten = flatten
+        self.dropout = dropout
+        self.weight_init = weight_init
+        self.out_channels = out_channels
+  
+    # Инициализация
+    def _setup_layer(self, input_: ndarray) -> ndarray:
+
+        self.params = []
+        in_channels = input_.shape[1]
+
+        if self.weight_init == "glorot":
+            scale = 2/(in_channels + self.out_channels)
+        else:
+            scale = 1.0
+
+        conv_param = np.random.normal(loc=0,
+                                      scale=scale,
+                                      size=(input_.shape[1],  # input channels
+                                     self.out_channels,
+                                     self.param_size,
+                                     self.param_size))
+
+        self.params.append(conv_param)
+
+        self.operations = []
+        self.operations.append(Conv2D_Op(conv_param))
+        self.operations.append(self.activation)
+
+        """
+        В зависимости от того, хотим ли мы передавать выходные данные этого
+        слоя в другой сверточный слой или в полносвязный связанный слой для
+        предсказаний, применятся (или нет) операция flatten.
+        """
+        if self.flatten:
+            self.operations.append(Flatten())
+
+        if self.dropout < 1.0:
+            self.operations.append(Dropout(self.dropout))
+
+        return None		
